dadcf4e522805622ef9b64f2e2ebed225221a08c
Limbo <admin@learninglimb.com>
2017-02-08 21:39:00 -0500

    Trying bubble-gum-sans font

--- src/app/components/home/index.ts
+++ src/app/components/home/index.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 
+declare var $;
+
 @Component({
     selector: 'home',
     templateUrl: './template.html',
     styleUrls: ['./styles.scss']
 })
 export class HomeComponent {
+
+    build() {
+
+    }
+
+    learn() {
+        
+    }
 }
