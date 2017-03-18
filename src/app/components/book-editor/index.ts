ad10c8205d01d26c3c6520f410389432d26fa234
Limbo <admin@learninglimb.com>
2017-03-18 11:18:21 -0400

    Added image uploader

--- src/app/components/book-editor/index.ts
+++ src/app/components/book-editor/index.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, NgZone } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { BookService, UserService } from '../../services';
@@ -10,26 +10,41 @@ import { Book } from '../../models';
     styleUrls: ['./styles.scss']
 })
 export class BookEditorComponent implements OnInit {
+    private reader: FileReader = new FileReader();
+
     bookId: string;
     book: Observable<Book>;
+    model: any = {};
     skinColors = ['white', 'brown', 'black'];
     hairColors = ['yellow', 'brown', 'black', 'orange'];
 
     selectedHairColor: string;
     selectedSkinColor: string;
+    selectedImageData = '/assets/Portrait_placeholder.png';
 
     constructor(
         private route: ActivatedRoute,
         private router: Router,
         private bookService: BookService,
-        public userService: UserService
+        public userService: UserService,
+        private zone: NgZone
     ) { }
 
     ngOnInit() {
+        this.reader.onload = (e) => {
+         this.zone.run(() => {
+             this.selectedImageData = e.target['result'];
+         }); 
+        }
         this.book = this.route.params.switchMap(params => {
             this.bookId = params['id'];
             return this.bookService.get(this.bookId);
         });
+    }
+
+    updateImage(e) {
+        this.model.image = e.target.files[0];
+        this.reader.readAsDataURL(this.model.image);
     }
 
 }
